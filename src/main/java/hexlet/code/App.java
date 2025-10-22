6dd356570bee648acc0815c0c3a3709f09d37277
Met-s <den_va@mail.ru>
2025-10-22 17:38:54 +0300

    New 14

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,12 +1,26 @@
 package hexlet.code;
 
+import java.util.logging.Logger;
+
+
 public class App {
+
+    static Logger logger = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
 
         String first = "Hello";
         String second = "World";
+        String result = first + ", " + second + "?*";
 
-        System.out.println(first + ", " + second + "?*");
+        logger.info(result);
+
+        int a = 10;
+        int b = 0;
+        while (b < a) {
+            b++;
+            System.out.println(b + result);
+        }
 
 
     }
