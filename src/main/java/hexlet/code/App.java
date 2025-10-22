e7e08796bf1213f3643be55cf99394196fc66a3d
Met-s <den_va@mail.ru>
2025-10-22 13:29:32 +0300

    New2

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,6 +4,14 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        int a = 10;
+        int b = 0;
+
+        while (b < a) {
+            b++;
+            System.out.println(b);
+        }
+
 
     }
 }
