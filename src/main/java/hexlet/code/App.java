a5426ff83d6f79c28550f94e51298cc9b16a3079
Met-s <den_va@mail.ru>
2025-10-22 16:20:25 +0300

    New 8

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,20 +2,11 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        int a = 5;
-        int b = 6;
-        int c = a + b;
-        System.out.println(c);
+        String first = "Hello";
+        String second = "World";
 
-//        int a = 10;
-//        int b = 0;
-//
-////        while (b < a) {
-////            b++;
-////            System.out.println(b);
-////        }
+        System.out.println(first + ", " + second + "!");
 
 
     }
