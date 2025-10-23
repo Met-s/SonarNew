2870d9d7229b2e4d53564c684ad65266db11e6ef
Met-s <den_va@mail.ru>
2025-10-23 11:01:28 +0300

    String 3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -14,26 +14,5 @@ public class App {
         String result = first + ", " + second + "?*";
 
         logger.info(result);
-
-        int a = 10;
-        int b = 0;
-        while (b < a) {
-            b++;
-            int c = a + b;
-            System.out.println(c);
-        }
-
-        String st = "Hello World";
-        logger.info(st);
-        var rs = st.charAt(3);
-        logger.info(String.valueOf(rs));
-
-        var length = st.length();
-        logger.info(String.valueOf(length));
-
-
-
-
-
     }
 }
